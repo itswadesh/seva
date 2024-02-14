68643484c390bbf5db3d77334385d14e7744c71c
ritikranjan12 <ritik123453@gmail.com>
2024-02-14 12:38:36 +0530

    fix:limit to 20 and ordered

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,9 +1,10 @@
 import { db } from "$lib/db/server";
 import {SangatData} from '$lib/db/schema'
+import { desc } from "drizzle-orm";
 export const load = async () => {
 
-    const result = await  db.select().from(SangatData);
-	return {
+    const result = await db.select().from(SangatData).orderBy(desc(SangatData.ID)).limit(20);
+    return {
         result
-	};
+    };
 };

aa4d774d0d61b5b325580d29a8b46c219dd30ce4
Swadesh Behera <itswadesh@gmail.com>
2024-02-22 11:53:59 +0530

    -

--- src/drizzle.config.ts
+++ src/drizzle.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from 'drizzle-kit';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
 const { DATABASE_URL } = process.env;
 if (!DATABASE_URL) {
 	throw new Error('No url');
